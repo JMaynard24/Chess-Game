04cba7093849bad0cfe4b713d3ad5ffc7848005d
Elizabeth Dooley <71342667+spacegirl43@users.noreply.github.com>
2020-11-17 09:28:42 -0600

    Add files via upload

---
+++ chessgame/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chessgame;
+
+import chessgame.gui.MenuSystem;
+import javafx.application.Application;
+
+/**
+ *
+ * @author eddpo
+ */
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(MenuSystem.class, args);
+  }
+}
